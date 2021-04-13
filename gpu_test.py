0fb1b4211a0b3c4169f89400b4c0bf9735d32b5c
blaze1522 <chakrabarty.goirik@gmail.com>
2021-04-14 02:11:47 +0530

    Checking the presence of GPU

--- gpu_test.py
+++ gpu_test.py
@@ -2,9 +2,19 @@
 import keras
 import tensorflow as tf
 
+
 # %%
-config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 1}) 
+print(keras.__version__)
+print(tf.__version__)
+
+# %% 
+print("Num CPU Available: ", len(tf.config.experimental.list_physical_devices('CPU')))
+# %%
+config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 1})
 # %%
 sess = tf.Session(config=config) 
 # %%
 keras.backend.set_session(sess)
+# %%
+config=tf.ConfigProto(log_device_placement=True)
+# %%
